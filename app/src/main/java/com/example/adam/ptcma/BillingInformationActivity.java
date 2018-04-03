35dd0e0aa4619fb20a7abcc891ab54b78e3e6529
Adam Burford <adamburford@gmail.com>
2018-04-02 21:38:13 -0400

    Added layouts from wireframe models

---
+++ app/src/main/java/com/example/adam/ptcma/BillingInformationActivity.java
@@ -0,0 +1,13 @@
+package com.example.adam.ptcma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BillingInformationActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_billing_information);
+	}
+}
