5bbdbfdd2a9306a72127c3022d98c649d8a5c52f
Adam Burford <adamburford@gmail.com>
2018-04-22 12:48:54 -0400

    Updated add customer screen to include working picture capture Saves picture to device storage

--- app/src/main/java/com/example/adam/ptcma/EditCustomerActivity.java
+++ app/src/main/java/com/example/adam/ptcma/EditCustomerActivity.java
@@ -1,11 +1,24 @@
 package com.example.adam.ptcma;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class EditCustomerActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,6 +27,7 @@ public class EditCustomerActivity extends AppCompatActivity implements View.OnCl
 	private EditText customerPhone;
 	private EditText customerEmail;
 	private EditText customerAddress;
+	private ImageView mImageView;
 
 	private DBManager dbManager;
 
@@ -28,6 +42,14 @@ public class EditCustomerActivity extends AppCompatActivity implements View.OnCl
 		customerAddress = (EditText) findViewById(R.id.editText5);
 
 		saveButton = (Button) findViewById(R.id.button);
+
+		mImageView = (ImageView) findViewById(R.id.imageButton);
+		mImageView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				takePicture();
+			}
+		});
 
 		dbManager = new DBManager(this);
 		dbManager.open();
@@ -50,5 +72,85 @@ public class EditCustomerActivity extends AppCompatActivity implements View.OnCl
 				startActivity(intent);
 				break;
 		}
+	}
+
+	private void takePicture()	{
+		Toast.makeText(getApplicationContext(), "Capture Picture", Toast.LENGTH_SHORT).show();
+		dispatchTakePictureIntent();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+			setPic();
+		}
+	}
+
+	String mCurrentPhotoPath;
+
+	private File createImageFile() throws IOException {
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		String imageFileName = "JPEG_" + timeStamp + "_";
+		File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+		File image = File.createTempFile(
+				imageFileName,  /* prefix */
+				".jpg",         /* suffix */
+				storageDir      /* directory */
+		);
+
+		// Save a file: path for use with ACTION_VIEW intents
+		mCurrentPhotoPath = image.getAbsolutePath();
+		return image;
+	}
+
+	static final int REQUEST_TAKE_PHOTO = 1;
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+
+	private void dispatchTakePictureIntent() {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		// Ensure that there's a camera activity to handle the intent
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			// Create the File where the photo should go
+			File photoFile = null;
+			try {
+				photoFile = createImageFile();
+			} catch (IOException ex) {
+				Toast.makeText(getApplicationContext(), "Error occurred while creating the File", Toast.LENGTH_SHORT).show();
+				// Error occurred while creating the File
+			}
+			// Continue only if the File was successfully created
+			if (photoFile != null) {
+				Uri photoURI = FileProvider.getUriForFile(this,
+						"com.example.adam.ptcma.fileprovider",
+						photoFile);
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+				startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+			}
+		}
+	}
+
+	private void setPic() {
+		// Get the dimensions of the View
+		int targetW = mImageView.getWidth();
+		int targetH = mImageView.getHeight();
+
+		// Get the dimensions of the bitmap
+		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+		bmOptions.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+		int photoW = bmOptions.outWidth;
+		int photoH = bmOptions.outHeight;
+
+		// Determine how much to scale down the image
+		int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+		// Decode the image file into a Bitmap sized to fill the View
+		bmOptions.inJustDecodeBounds = false;
+		bmOptions.inSampleSize = scaleFactor;
+		bmOptions.inPurgeable = true;
+
+		Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+		mImageView.setImageBitmap(bitmap);
 	}
 }
